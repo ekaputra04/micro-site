d6f33a9314d465d60fb13e33c45873dd6329f142
ekaputra04 <ekaputrajuniawan@gmail.com>
2025-01-19 00:25:26 +0800

    refactor: refactor ThemeData export to Consts; update SettingView component imports and enhance tsconfig paths for better module resolution

--- types/Consts.ts
+++ types/Consts.ts
@@ -1,4 +1,5 @@
 import { AccordionItem } from "./AccordionItem";
+import { ThemeDataType } from "./ThemeData";
 import { FileType, MainInformationType } from "./Types";
 
 export enum COLOR {
@@ -121,3 +122,106 @@ export const initialMainInformation: MainInformationType = {
   description: "",
   iconImage: "",
 };
+
+export const ThemeData: ThemeDataType[] = [
+  {
+    name: "Abstract Flat Modern",
+    url: "/images/abstract-flat-modern.png",
+  },
+  {
+    name: "Abstract Selene",
+    url: "/images/abstract-selene.png",
+  },
+  {
+    name: "Abstract Zephyra",
+    url: "/images/abstract-zephyra.png",
+  },
+  {
+    name: "Astronom",
+    url: "/images/astronom.png",
+  },
+  {
+    name: "Batik",
+    url: "/images/batik.png",
+  },
+  {
+    name: "Christmas Holy",
+    url: "/images/christmas-holy.png",
+  },
+  {
+    name: "Corporation",
+    url: "/images/corporation.png",
+  },
+  {
+    name: "Crimson Harmony",
+    url: "/images/crimson-harmony.png",
+  },
+  {
+    name: "Darkness Hallowen",
+    url: "/images/darkness-hallowen.png",
+  },
+  {
+    name: "Dusks Cloud",
+    url: "/images/dusks-cloud.png",
+  },
+  {
+    name: "Grocery",
+    url: "/images/grocery.png",
+  },
+  {
+    name: "Hallowen Day",
+    url: "/images/hallowen-day.png",
+  },
+  {
+    name: "Hallowen Party",
+    url: "/images/hallowen-party.png",
+  },
+  {
+    name: "Ignite The Future",
+    url: "/images/ignite-the-future.png",
+  },
+  {
+    name: "Indonesian",
+    url: "/images/indonesian.png",
+  },
+  {
+    name: "Juicy",
+    url: "/images/juicy.png",
+  },
+  {
+    name: "Lunar Prosperity",
+    url: "/images/lunar-prosperity.png",
+  },
+  {
+    name: "Merry Christmas",
+    url: "/images/merry-christmas.png",
+  },
+  {
+    name: "New Year New You",
+    url: "/images/new-year-new-you.png",
+  },
+  {
+    name: "Rainbowwave Marble",
+    url: "/images/rainbowwave-marble.png",
+  },
+  {
+    name: "Self",
+    url: "/images/self.png",
+  },
+  {
+    name: "Serene Dawn",
+    url: "/images/serene-dawn.png",
+  },
+  {
+    name: "Soft Pastel",
+    url: "/images/soft-pastel.png",
+  },
+  {
+    name: "Travel",
+    url: "/images/travel.png",
+  },
+  {
+    name: "Trick or Treat Hallowen",
+    url: "/images/trick-or-treat-hallowen.png",
+  },
+];
