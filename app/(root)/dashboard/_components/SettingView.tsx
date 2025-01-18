41325f640459f16727878183b3aaa6fac60b915e
ekaputra04 <ekaputrajuniawan@gmail.com>
2025-01-19 00:03:55 +0800

    feat: move ThemeData definition to SettingView component; update import in SettingView and remove redundant ThemeData export from type definition

--- app/(root)/dashboard/_components/SettingView.tsx
+++ app/(root)/dashboard/_components/SettingView.tsx
@@ -10,7 +10,110 @@ import useFileStore from "@/hooks/useFileStore";
 import { toast } from "sonner";
 import { Textarea } from "@/components/ui/textarea";
 import { deleteFile } from "@/utils/imageUtils";
-import { ThemeData } from "@/types/ThemeData";
+import { ThemeDataType } from "@/types/ThemeData";
+
+export const ThemeData: ThemeDataType[] = [
+  {
+    name: "Abstract Flat Modern",
+    url: "/images/abstract-flat-modern.png",
+  },
+  {
+    name: "Abstract Selene",
+    url: "/images/abstract-selene.png",
+  },
+  {
+    name: "Abstract Zephyra",
+    url: "/images/abstract-zephyra.png",
+  },
+  {
+    name: "Astronom",
+    url: "/images/astronom.png",
+  },
+  {
+    name: "Batik",
+    url: "/images/batik.png",
+  },
+  {
+    name: "Christmas Holy",
+    url: "/images/christmas-holy.png",
+  },
+  {
+    name: "Corporation",
+    url: "/images/corporation.png",
+  },
+  {
+    name: "Crimson Harmony",
+    url: "/images/crimson-harmony.png",
+  },
+  {
+    name: "Darkness Hallowen",
+    url: "/images/darkness-hallowen.png",
+  },
+  {
+    name: "Dusks Cloud",
+    url: "/images/dusks-cloud.png",
+  },
+  {
+    name: "Grocery",
+    url: "/images/grocery.png",
+  },
+  {
+    name: "Hallowen Day",
+    url: "/images/hallowen-day.png",
+  },
+  {
+    name: "Hallowen Party",
+    url: "/images/hallowen-party.png",
+  },
+  {
+    name: "Ignite The Future",
+    url: "/images/ignite-the-future.png",
+  },
+  {
+    name: "Indonesian",
+    url: "/images/indonesian.png",
+  },
+  {
+    name: "Juicy",
+    url: "/images/juicy.png",
+  },
+  {
+    name: "Lunar Prosperity",
+    url: "/images/lunar-prosperity.png",
+  },
+  {
+    name: "Merry Christmas",
+    url: "/images/merry-christmas.png",
+  },
+  {
+    name: "New Year New You",
+    url: "/images/new-year-new-you.png",
+  },
+  {
+    name: "Rainbowwave Marble",
+    url: "/images/rainbowwave-marble.png",
+  },
+  {
+    name: "Self",
+    url: "/images/self.png",
+  },
+  {
+    name: "Serene Dawn",
+    url: "/images/serene-dawn.png",
+  },
+  {
+    name: "Soft Pastel",
+    url: "/images/soft-pastel.png",
+  },
+  {
+    name: "Travel",
+    url: "/images/travel.png",
+  },
+  {
+    name: "Trick or Treat Hallowen",
+    url: "/images/trick-or-treat-hallowen.png",
+  },
+];
 
 interface SettingViewProps {
   type: "create" | "edit";
